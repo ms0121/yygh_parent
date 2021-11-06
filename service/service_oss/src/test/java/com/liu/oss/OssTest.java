25c0e86256ebf3171b1944ccd75399e6be3acd7c
ms0121 <1132601565@qq.com>
2021-11-06 15:28:34 +0800

    第二次提交

--- service/service_oss/src/test/java/com/liu/oss/OssTest.java
+++ service/service_oss/src/test/java/com/liu/oss/OssTest.java
@@ -11,10 +11,11 @@ public class OssTest {
     public static void main(String[] args) {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
-        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-        String accessKeyId = "LTAI5tR6nT781Js4Wmks6BCH";
-        String accessKeySecret = "YWG3ojS7wOYPy05HcZdCsMErlgUdXX";
-        String bucketName = "yygh-oss-test";
+        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。
+        // 强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
+        String accessKeyId = "";
+        String accessKeySecret = "";
+        String bucketName = "";
 
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
